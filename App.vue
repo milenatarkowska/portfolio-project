224031d66d3a17b23360f8f002567eeb9db87a54
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-22 13:43:39 +0200

    Layout created

--- App.vue
+++ App.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="app">
     <Header />
+    <main class="main">
+      <HeroPageView />
+    </main>
     <Footer />
   </div>
 </template>
@@ -8,4 +11,18 @@
 <script setup>
 import Header from './components/comp-header.vue'
 import Footer from './components/comp-footer.vue'
+import HeroPageView from '@/views/HeroPageView.vue'
 </script>
+
+<style scoped>
+.app{
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main{
+  display: flex;
+  flex: 1;
+  height: 100%;
+}
+</style>
