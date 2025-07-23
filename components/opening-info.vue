57848130ab65d406dfda275e1963b324fcace3eb
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-23 14:28:13 +0200

    Styles corrected

--- components/opening-info.vue
+++ components/opening-info.vue
@@ -3,8 +3,7 @@
     <div class="left-side">
       <h1><b>Milena Tarkowska</b></h1>
       <h3>Computer Science Student</h3>
-      <p>Currently pursuing a bachelor's degree at
-        <br> <b>Polish-Japanese Academy of Information Technology</b></p>
+      <p>Currently pursuing a bachelor's degree at<b> Polish-Japanese Academy of Information Technology</b></p>
     </div>
     <div class="right-side">
       <p><i class="fas fa-map-marker-alt"></i> Warsaw, Poland</p>
@@ -16,9 +15,10 @@
 <style scoped>
 .opening-info{
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
   align-items: center;
   flex: 1;
-  padding: 1.5rem;
+  margin: 2rem;
 }
 </style>
