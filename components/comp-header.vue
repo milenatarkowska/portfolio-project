82dfe3ec9b23195c05e3d0f8fd559fd34b677572
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-24 12:50:34 +0200

    Added language change swap

--- components/comp-header.vue
+++ components/comp-header.vue
@@ -3,8 +3,9 @@
     <div class="navbar">
       <NavBar />
     </div>
-    <div class="switch">
+    <div class="settings">
       <ColorModeSwitch />
+     <LangChangeComp />
     </div>
   </div>
 </template>
@@ -17,9 +18,16 @@
   margin-left: 1rem;
   margin-right: 1rem;
 }
+.settings {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+}
 </style>
 
 <script setup>
 import ColorModeSwitch from "./color-modes-switch.vue";
 import NavBar from "./comp-navbar.vue";
+import LangChangeComp from "./lang-change-comp.vue";
 </script>
---
+++ components/lang-change-comp.vue
@@ -0,0 +1,7 @@
+<template>
+  <label class="swap">
+    <input type="checkbox" />
+    <div class="swap-on">ENG</div>
+    <div class="swap-off">PL</div>
+  </label>
+</template>
