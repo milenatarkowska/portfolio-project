d63aa060fea7b5c30564fa99ef8647505f674c11
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-24 12:54:50 +0200

    Added navbar styling

--- components/comp-navbar.vue
+++ components/comp-navbar.vue
@@ -1,7 +1,17 @@
 <template>
   <nav class="navbar">
+    <router-link to="/">Main Page</router-link>
     <router-link to="about_me">About Me</router-link>
 
     <!-- tu dodac kolejne karty -->
   </nav>
 </template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1rem;
+}
+</style>
