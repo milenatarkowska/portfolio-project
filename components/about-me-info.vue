3c24d4bb88572c68be1af02dc302b51f5b5608ba
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-28 12:13:53 +0200

    Translation added (eng-pl)

--- components/about-me-info.vue
+++ components/about-me-info.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="about-me">
     <section class="left-title">
-      <h1>ABOUT ME</h1>
+      <h1><b>{{ t('aboutMe') }}</b></h1>
     </section>
     <section class="right-info">
       <p>
-        Third-year Computer Science student at the Polish-Japanese Academy of Information Technology in Warsaw, actively seeking internship and entry-level opportunities in the IT industry.
+        {{t('aboutFirstSentence')}}
         <br><br>
-        I'm passionate about expanding my practical knowledge and applying my skills in real-world scenarios. My key areas of interest include Software Development, Cloud Engineering, DevOps, and Security. Alongside my academic studies, I continually develop my expertise through online courses and self-directed learning.
+        {{t('aboutSecondSentence')}}
         <br><br>
-        With strong analytical skills, a problem-solving mindset, and a creative approach, I'm eager to contribute to innovative projects and grow as a professional in the tech industry.
+        {{t('aboutThirdSentence')}}
+        <br><br>
+        {{t('aboutFourthSentence')}}
       </p>
     </section>
   </div>
@@ -40,3 +42,8 @@
 
 
 </style>
+
+<script setup>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
--- i18n.js
+++ i18n.js
@@ -11,6 +11,11 @@ const messages = {
     email: ' milenatarkowska1@gmail.com',
     home: 'Home',
     about: 'About',
+    aboutMe: 'About me',
+    aboutFirstSentence: 'Third-year Computer Science student at the Polish-Japanese Academy of Information Technology in Warsaw, actively seeking internship and entry-level opportunities in the IT industry.',
+    aboutSecondSentence: 'My key areas of interest include Software Development, Cloud Engineering, DevOps, and Security.',
+    aboutThirdSentence: 'I’m deeply committed to enhancing my practical skills and applying them in real-world projects. Alongside my academic studies, I proactively deepen my knowledge through online courses and independent learning.',
+    aboutFourthSentence: 'With strong analytical skills, a problem-solving mindset, and a creative approach, I\'m eager to contribute to innovative projects and grow as a professional in the tech industry.',
   },
   pl: {
     name: 'Milena Tarkowska',
@@ -20,7 +25,12 @@ const messages = {
     location: 'Warszawa, Polska',
     email: ' milenatarkowska1@gmail.com',
     home: 'Strona Główna',
-    about: 'O mnie'
+    about: 'O mnie',
+    aboutMe: 'O mnie',
+    aboutFirstSentence: 'Jestem studentką trzeciego roku Informatyki na Polsko-Japońskiej Akademii Technik Komputerowych w Warszawie i aktywnie poszukuję stażu oraz możliwości rozpoczęcia kariery zawodowej w branży IT.',
+    aboutSecondSentence: 'Moje główne obszary zainteresowań to: inżynieria oprogramowania, technologie chmurowe, DevOps i cyberbezpieczeństwo.',
+    aboutThirdSentence: 'Z dużym zaangażowaniem rozwijam swoje umiejętności praktyczne i wykorzystuję je w rzeczywistych projektach. Równolegle ze studiami systematycznie pogłębiam swoją wiedzę poprzez kursy online i samodzielną naukę.',
+    aboutFourthSentence: 'Dzięki silnym umiejętnościom analitycznym, nastawieniu na rozwiązywanie problemów oraz kreatywnemu podejściu, z entuzjazmem chcę angażować się w innowacyjne projekty i rozwijać się jako specjalistka w branży IT.',
   }
 }
 
