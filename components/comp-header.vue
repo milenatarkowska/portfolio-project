af1c89ea05266e028792878998e890114df262ab
milenatarkowska <milenatarkowska1@gmail.com>
2025-07-30 13:39:44 +0200

    Header height corrected

--- components/comp-header.vue
+++ components/comp-header.vue
@@ -12,13 +12,14 @@
 
 <style scoped>
 .header {
-  position:sticky;
+  position: sticky;
   top: 0;
   display: flex;
   justify-content: flex-end;
   align-items: center;
   padding-left: 1rem;
   padding-right: 1rem;
+  height: 4rem;
 }
 .settings {
   display: flex;
